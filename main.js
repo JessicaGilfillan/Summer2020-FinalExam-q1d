26dc8247cf140314d7758058404c5ae81b959dcb
JessicaGilfillan <49848402+JessicaGilfillan@users.noreply.github.com>
2020-07-29 20:35:07 -0400

    Add files via upload

---
+++ main.js
@@ -0,0 +1,13 @@
+/* 
+
+Using the image displayed in index.html, create the following using JavaScript. 
+
+When the user hovers over the image displayed, the image changes to a img2.jpg 
+
+When the user hovers over the image displayed, a new div is added to the page. Inside the div, there is an H1 that says “Good Luck On Your Final Exam!”.
+
+When the user hovers away from the image, the background colour changes to #C0DE25 and the H1 disappears. 
+
+
+*/
+
